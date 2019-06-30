4048150c78cdcd28f86bc8b3c02c4a5a49e0f2b0
shekhars-realm <sbhattacharaya099@gmail.com>
2019-06-30 17:31:48 +0200

    signup route added

--- functions/index.js
+++ functions/index.js
@@ -59,6 +59,17 @@ app.post('/shout', (req, res) => {
   })
 });
 
+const isEmpty = (string) => {
+  if(string.trim() === '') return true
+  else return false
+}
+
+const isEmail = (email) => {
+  const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if(email.match(emailRegEx)) return true
+  else return false
+}
+
 //signup route
 app.post('/signup', (req, res) => {
   const newUser = {
@@ -68,7 +79,22 @@ app.post('/signup', (req, res) => {
     handle: req.body.handle
   };
 
+  let errors = {};
+
+  if(isEmpty(newUser.email)) {
+    errors.email = 'Email must not be empty'
+  } else if(!isEmail(newUser.email)) {
+    errors.email = 'Must be a valid email address'
+  }
+
+  if(isEmpty(newUser.password)) errors.password = 'Must not be empty'
+  if(newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Passowrds must be the same'
+  if(isEmpty(newUser.handle)) errors.handle = 'Must not be empty'
+
+  if(Object.keys(errors).length > 0) return res.status(400).json(errors)
+
   //validate data
+  let token, user;
   db.doc(`/users/${newUser.handle}`).get()
   .then((doc) => {
     if(doc.exists) {
@@ -78,10 +104,21 @@ app.post('/signup', (req, res) => {
     }
   })
   .then((data) => {
+    userId = data.user.uid
     return data.user.getIdToken();
   })
-  .then((token) => {
-    return res.status(200).json({token: token})
+  .then((userToken) => {
+    token = userToken
+    const userCredentials = {
+      handle: newUser.handle,
+      email: newUser.email,
+      createdAt: new Date().toISOString(),
+      userId
+    }
+    return db.doc(`/users/${newUser.handle}`).set(userCredentials)
+  })
+  .then((data) => {
+    return res.status(201).json({token})
   })
   .catch((err) => {
     if(err.code === 'auth/email-already-in-use') {
